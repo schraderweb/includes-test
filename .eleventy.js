b55111fb2191b661f3e91f83d16a78bb4857b72a
Nitin Kumar <nitin2953jio@gmail.com>
2023-02-07 22:32:12 +0530

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+	return {
+		templateFormats: ["njk", "md", "html"],
+		markdownTemplateEngine: "njk",
+
+		dir: {
+			includes: "_includes",
+			input: "src",
+			output: "dist"
+		}
+	}
+}
