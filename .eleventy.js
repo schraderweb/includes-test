e41f126e6546dc72e6c2932f7a9e7b1f42952952
Nitin Kumar <nitin2953jio@gmail.com>
2023-02-08 21:30:39 +0530

    added Bootslander pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
 module.exports = function (eleventyConfig) {
+
+	eleventyConfig.addShortcode('renderlayoutblock', function(name) {
+		return (this.page.layoutblock || {})[name] || ''
+	})
+	eleventyConfig.addPairedShortcode('layoutblock', function(content, name) {
+		if (!this.page.layoutblock) this.page.layoutblock = {}
+		this.page.layoutblock[name] = content
+		return ''
+	})
+
+	eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+	eleventyConfig.addPassthroughCopy({ "src/assets": "./assets" });
+
 	return {
 		templateFormats: ["njk", "md", "html"],
 		markdownTemplateEngine: "njk",
